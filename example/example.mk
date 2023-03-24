83b04f8736172e48ac43755e9bf2d919e8d28d9f
Timothy Murphy <tnmurphy@gmail.com>
2023-03-24 08:17:10 +0000

    comment on the example to make it a little clearer

--- example/example.mk
+++ example/example.mk
@@ -8,15 +8,30 @@ SOURCES:=proga.c progb.c
 OBJECTS:=$(SOURCES:.c=.o)
 
 
+# This section causes a hash generating/loading function (siphash24)
+# from extramake to be built and loaded into GNU Make: 
 include ../xtra.mk
 XTRA_OUTPUTDIR:=.
 XTRA_SOURCE:=..
 -load $(XTRA_OUTPUTDIR)/hash$(XTRA_EXT)
 
+# Now we're using the siphash function that was loaded above to
+# create a unique version that is based on the list of source
+# files. 
+#
+# Rationale: We want to rebuild the library not only when the source
+# files change but when a new source file is added or one is removed.
+# Even if that list of source files is very long the 
+# hash of it will have a constant size (which is why we don't just
+# concatenate all the filenames to make a version):
+#
 LIBVERSION:=$(siphash24 $(SOURCES))
 LIBNAME:=prog.$(LIBVERSION).so
 LIBSHORTNAME:=prog.so
 
+# link the library and force a short symbolic link to point to it
+# which other programs can use if they are not sensitive to some
+# specific version:
 $(LIBNAME): $(OBJECTS)
 	cc -o $@ $^ -shared
 	ln -sf $@ $(LIBSHORTNAME)
