2d67fa92388baf861853d10b8682ad8bc712fe62
Timothy Murphy <tnmurphy@gmail.com>
2025-06-19 16:20:46 +0100

    Test regex with a variable to see that it is actually being expanded.

--- test_regex.mk
+++ test_regex.mk
@@ -4,8 +4,10 @@
 HANDLE:=$(regcomp (b...).*)
 $(info HANDLE=$(HANDLE))
 
-$(info RESULT1=$(regexec $(HANDLE),i1235badabcdef))
-$(info RESULT2=$(regexec $(HANDLE),bad12869))
-$(info RESULT3=$(regexec $(HANDLE),abba))
+$(info RESULT1=$(regexec $(HANDLE),i1235badabcdef)) # badabcdef bada
+$(info RESULT2=$(regexec $(HANDLE),bad12869)) # bad12869 bad1
+$(info RESULT3=$(regexec $(HANDLE),abba)) # empty
+STR:=abad,0987654321
+$(info RESULT4=$(regexec $(HANDLE),$(STR)))
 
 
