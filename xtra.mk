eb17d0b1724f6940cf63e8b3deb357c24401c529
Timothy Murphy <tnmurphy@gmail.com>
2025-07-06 18:13:14 +0100

    Automatically try to use the correct make headers for the OS on which we are building. Make 4.4.1 is close enough to 4.4

--- xtra.mk
+++ xtra.mk
@@ -3,7 +3,8 @@
 # Linux/unix for now:
 XTRA_EXT=.so
 
-XTRA_MAKEHEADERS ?= ./include/gmake421
+XTRA_INCLUDE_VERSION=$(subst .,,$(MAKE_VERSION))
+XTRA_MAKEHEADERS ?= ./include/gmake$(XTRA_INCLUDE_VERSION)
 XTRA_OUTPUTDIR ?= .
 XTRA_CFLAGS ?= -fPIC -O2 -g -Wall -Wextra -Werror
 XTRA_SOURCE ?= .
